2242cbe45ab1044a0cbcbe686d5041096d42d08e
fatimaminhas <f.minhas394@gmail.com>
2023-10-04 17:13:08 -0400

    added inquirer functions

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,31 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employees (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  manager_id VARCHAR(30) NOT NULL,
-  FOREIGN KEY (roles)
-  REFERENCES roles(id)
-  ON DELETE SET NULL
+  department_name VARCHAR(30) NOT NULL
+  -- ON DELETE SET NULL
 );
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  FOREIGN KEY (departments)
+  department_id INT,
+  FOREIGN KEY (department_id)
   REFERENCES departments(id)
   ON DELETE SET NULL
 );
 
-CREATE TABLE departments (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
-  ON DELETE SET NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  -- ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-const express = require('express');
-// Import and require mysql2
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -19,23 +11,73 @@ const db = mysql.createConnection(
     password: 'password',
     database: 'employees_db'
   },
-  console.log(`Connected to the employees_db database.`)
+  // console.log(`Connected to the employees_db database.`)
 );
 
-// // Query database using COUNT() and GROUP BY
-// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-//   console.log(results);
-// });
+function start() {
+inquirer
+  .prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'menu',
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department'],
+      },
+  ])
+  .then((response) => {
+    if (response.menu === 'View Departments') {
+      viewDepartments();
+    } else if (response.menu === 'Add Department') {
+      addDepartment();
+    } else if (response.menu === 'View Roles') {
+      viewRoles();
+    } else if (response.menu === 'View Employees') {
+      viewEmployees();
+    }
+  });
+}
 
-// // Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-// db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-//   console.log(results);
-// });
+function viewDepartments() {
+  db.query('SELECT * FROM departments', function (err, results) {
+    console.table(results);
+    start();
+  });
+}
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+function viewRoles() {
+  db.query('SELECT * FROM roles', function (err, results) {
+    console.table(results);
+    start();
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function viewEmployees() {
+  db.query('SELECT * FROM employees', function (err, results) {
+    console.table(results);
+    start();
+  });
+}
+
+function addDepartment() {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Type in department name.',
+      name: 'department',
+    }
+  ])
+  .then((response) => {
+    console.log(response)
+    const sql = `INSERT INTO departments (department_name)
+    VALUES (?)`;
+    const params = [response.department];
+  
+  db.query(sql, params, (err, result) => {
+   console.log("Success!")
+  });
+  })
+  
+}
+
+start();
